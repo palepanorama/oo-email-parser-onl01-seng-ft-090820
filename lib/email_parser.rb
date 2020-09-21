8bd438566c851684540e5c4c4360dd156bf6c603
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-21 14:53:57 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,8 @@ class EmailAddressParser
   
   attr_accessor :email_addresses
   
-  def initialize 
+  def initialize(email_addresses) 
+    @email_addresses = email_addresses  
   end 
   
   def parse(parse)
